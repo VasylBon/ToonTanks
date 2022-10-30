1b1f9a2b20be685bf9ad5ad71906616998205dd1
Vasyl Bonkalo <vas.bon.design@gmail.com>
2022-10-30 23:51:18 +0200

    Draw debug sphere under mouse cursor

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -6,6 +6,7 @@
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
 ATank::ATank()
 {
@@ -26,6 +27,46 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
 	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PlayerControllerRef = Cast<APlayerController>(GetController());
+
+	DrawDebugSphere(
+		GetWorld(), 
+		GetActorLocation() + FVector(0.f, 0.f, 200.f),
+		100.f, 
+		12, 
+		FColor::Red,
+		false,
+		-1.0f);
+}
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (PlayerControllerRef)
+	{
+		FHitResult HitResult;
+		PlayerControllerRef->GetHitResultUnderCursor(
+			ECollisionChannel::ECC_Visibility, 
+			false, 
+			HitResult);
+		DrawDebugSphere(
+			GetWorld(),
+			HitResult.ImpactPoint,
+			25.f,
+			12,
+			FColor::Red,
+			false,
+			-1.0f);
+	}
 }
 
 void ATank::Move(float Value)
